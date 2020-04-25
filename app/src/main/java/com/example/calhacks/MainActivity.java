02c2438bd2efc74c9e717a7e1268b90fdaac302f
ramyayyagari <ramyayyagari@gmail.com>
2020-04-25 12:07:33 -0700

    Committing my changes

--- app/src/main/java/com/example/calhacks/MainActivity.java
+++ app/src/main/java/com/example/calhacks/MainActivity.java
@@ -1,7 +1,6 @@
-package com.androidedx.calhacksapp;
+package com.example.calhacks;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
--- app/src/test/java/com/example/calhacks/ExampleUnitTest.java
+++ app/src/test/java/com/example/calhacks/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.androidedx.calhacksapp;
+package com.example.calhacks;
 
 import org.junit.Test;
 
