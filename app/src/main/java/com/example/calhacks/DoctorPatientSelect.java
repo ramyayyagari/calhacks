61c3b6874f27dfb6a2b230851c9f854e3a07c4d3
menahassan <menah9@gmail.com>
2020-04-25 12:26:05 -0700

    add doctor-patient select and patient registration

---
+++ app/src/main/java/com/example/calhacks/DoctorPatientSelect.java
@@ -0,0 +1,27 @@
+package com.example.calhacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DoctorPatientSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_patient_select);
+
+        Button patientButton = findViewById(R.id.patientbutton);
+        patientButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activity2Intent = new Intent(getApplicationContext(), PatientRegistration.class);
+                startActivity(activity2Intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/calhacks/PatientRegistration.java
@@ -0,0 +1,36 @@
+package com.example.calhacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class PatientRegistration extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    public Spinner spinner1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_registration);
+
+        spinner1 = (Spinner) findViewById(R.id.StatesMenu);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.states_array, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //num1
+        spinner1.setAdapter(adapter1);
+        spinner1.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //nothing
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+}
