6a7d477ff3b7339efcd659e229537262814f524b
menahassan <menah9@gmail.com>
2020-04-25 12:35:30 -0700

    update mainactivity.java

--- app/src/main/java/com/example/calhacks/MainActivity.java
+++ app/src/main/java/com/example/calhacks/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.calhacks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signupButton = findViewById(R.id.signup);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activity2Intent = new Intent(getApplicationContext(), DoctorPatientSelect.class);
+                startActivity(activity2Intent);
+            }
+        });
     }
 }
