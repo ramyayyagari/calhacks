6076a3b822e0d4f8eca2289a77292eb86b5fd5b2
madeleinerosesalem <64326967+madeleinerosesalem@users.noreply.github.com>
2020-04-25 21:58:42 -0700

    Updated Request Form

--- app/src/main/java/com/example/calhacks/RequestForm.java
+++ app/src/main/java/com/example/calhacks/RequestForm.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RequestForm extends AppCompatActivity {
-
+    public String description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
