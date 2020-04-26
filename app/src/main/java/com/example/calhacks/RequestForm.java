4c9d98d92a3b152797bc0b6e102ada7c0c194d31
madeleinerosesalem <64326967+madeleinerosesalem@users.noreply.github.com>
2020-04-25 23:32:59 -0700

    Updated Request Form

--- app/src/main/java/com/example/calhacks/RequestForm.java
+++ app/src/main/java/com/example/calhacks/RequestForm.java
@@ -2,12 +2,35 @@ package com.example.calhacks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class RequestForm extends AppCompatActivity {
+public class RequestForm extends AppCompatActivity implements OnItemSelectedListener {
     public String description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_form);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.SpecialtyMenu);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.specialty_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        //nothing
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
