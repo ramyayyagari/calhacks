41be6edd79c5e04b3f91824e9ee2e2feff2feb86
Venkata Ayyagari <vsayyagari@gmail.com>
2020-04-25 17:41:01 -0700

    Added list of doctors for patient home screen

---
+++ app/src/main/java/com/example/calhacks/DoctorDetailFragment.java
@@ -0,0 +1,61 @@
+package com.example.calhacks;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.calhacks.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Doctor detail screen.
+ * This fragment is either contained in a {@link DoctorListActivity}
+ * in two-pane mode (on tablets) or a {@link DoctorDetailActivity}
+ * on handsets.
+ */
+public class DoctorDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public DoctorDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.doctor_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.doctor_detail)).setText(mItem.details);
+        }
+
+        return rootView;
+    }
+}
