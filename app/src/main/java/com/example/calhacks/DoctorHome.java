7bdd4005cc01a9fc4ef6cd06e0d66f0d472dc1b0
madeleinerosesalem <64326967+madeleinerosesalem@users.noreply.github.com>
2020-04-25 18:45:51 -0700

    Added Doctor Home and Request Form

---
+++ app/src/main/java/com/example/calhacks/DoctorHome.java
@@ -0,0 +1,25 @@
+package com.example.calhacks;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DoctorHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_home);
+
+        Button declinePatientButton = findViewById(R.id.declinePatientButton);
+        //declinePatientButton.setOnClickListener(new View.OnClickListener() {
+            //public void onClick(View v) {
+                //Intent activity2Intent = new Intent(getApplicationContext(), PatientRegistration.class);
+               // startActivity(activity2Intent);
+           // }
+        //});
+        Button acceptPatientButton = findViewById(R.id.acceptPatientButton);
+    }
+}
